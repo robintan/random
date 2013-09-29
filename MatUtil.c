582869d2e09bee02878057b7514adf6370225a2a
Robin <robintan1993@gmail.com>
2013-09-29 22:59:26 +0800

    Floyd using MPI

---
+++ MatUtil.c
@@ -0,0 +1,83 @@
+#include "MatUtil.h"
+#include <mpi.h>
+
+inline int min(int a, int b) {
+	return (a != -1 && a < b) ? a : b;
+}
+
+void GenMatrix(int *mat, const size_t N)
+{
+	for(int i = 0; i < N; i ++)
+		for(int j = 0; j < N; j ++)
+			mat[i*N+j] = (i==j)?0:rand()%32 - 1;
+}
+
+bool CmpArray(const int *l, const int *r, const size_t eleNum)
+{
+	for(int i = 0; i < eleNum; i ++)
+		if(l[i] != r[i])
+		{
+			printf("ERROR: l[%d] = %d, r[%d] = %d\n", i, l[i], i, r[i]);
+			return false;
+		}
+	return true;
+}
+
+
+/*
+	Sequential (Single Thread) APSP on CPU.
+*/
+void ST_APSP(int *mat, const size_t N)
+{
+	for(int k = 0; k < N; k ++)
+		for(int i = 0; i < N; i ++)
+			for(int j = 0; j < N; j ++)
+			{
+				int i0 = i*N + j;	//i0 = mat[i][j]
+				int i1 = i*N + k;	//i1 = mat[i][k]
+				int i2 = k*N + j;	//i2 = mat[k][j]
+				if(mat[i1] != -1 && mat[i2] != -1)
+					mat[i0] = min(mat[i0], mat[i1] + mat[i2]);
+			}
+}
+/*
+  Multiple thread (Parallel) APSP on CPU
+*/
+void MT_APSP(int *part, const size_t N, MPI_Comm comm, int myrank, int p) {
+  int s = N/p;
+  int root, offset;
+  int *temp = (int*)malloc(sizeof(int)*N);
+	//printf("Local: %d\n",myrank);
+	//printMatrix(part,N,p);
+
+  for (int k = 0; k < N; k++) {
+    root = k/s;
+    if (myrank == root) {
+      offset = k - myrank*s;
+      for (int j = 0; j < N; j++) 
+				temp[j] = part[offset*N + j];
+    } 
+		//printf("Temp: %d\n",myrank);
+		//printMatrix(temp,N,N);
+    MPI_Bcast(temp, N, MPI_INT, root, comm);
+    for(int i = 0; i < s; i ++)
+			for(int j = 0; j < N; j ++) {
+				int i0 = i*N + j;
+				int i1 = i*N + k;
+				if (part[i1] != -1 && temp[j] != -1)
+					part[i0] = min(part[i0], part[i1] + temp[j]);
+			}
+  }
+}
+
+void printMatrix (int *mat, const size_t N, int p) {
+  for (int i = 0; i < N/p; i++) {
+    for(int j = 0; j < N ; j++) {
+      int value = mat[i*N + j];
+			printf("%d\t", value);
+    }
+    printf("\n");
+  }
+}
+
+
---
+++ floyd.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <sys/time.h>
+#include "MatUtil.h"
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		printf("Usage: test {N}\n");
+		exit(-1);
+	}
+	// Variable initialisation for Sequential and MPI
+	struct timeval start, end;
+	size_t N = atoi(argv[1]);
+	int* mat;
+	int* ref;
+	// Variable initialisation for MPI
+	int myrank;				//process rank
+	int p;				//number of processes
+	MPI_Comm comm = MPI_COMM_WORLD;
+	MPI_Init(&argc, &argv);
+	// Get the process rank
+  MPI_Comm_rank(comm, &myrank);
+  // Get the number of the processes
+  MPI_Comm_size(comm, &p);
+
+ 
+	//sequential computation
+	if (myrank == 0) {
+		
+		mat = (int*)malloc(sizeof(int)*N*N);
+		// generate a random matrix.
+		GenMatrix(mat, N);
+			
+		//printf("Init:\n");
+		//printMatrix(mat,N,1);
+		
+		// compute the reference result.
+		ref = (int*)malloc(sizeof(int)*N*N);
+		memcpy(ref, mat, sizeof(int)*N*N);
+		printf("Executing sequentially ...\n");
+		gettimeofday(&start,0);
+		//ST_APSP(ref, N);
+		gettimeofday(&end, 0);
+		//printf("Ref:\n");
+		//printMatrix(ref,N,1);
+		
+		printf("Sequential execution time = %ld usecs\n",
+			(end.tv_sec-start.tv_sec)*1000000+(end.tv_usec-start.tv_usec));
+	}
+
+	// parallel computation
+	// Broadcast N value
+	MPI_Bcast(&N, 1, MPI_INT, 0, comm);
+
+	int* localMatrix = (int*)malloc(sizeof(int)*N*N/p);
+	if (myrank == 0) {
+		printf("Executing in parallel ...\n");
+		gettimeofday(&start,0);
+	}
+	// Scatter data to p processors
+	MPI_Scatter(mat, N*N/p, MPI_INT, localMatrix, N*N/p, MPI_INT, 0, comm);
+
+	//printf("Local Matrix: %d\n", myrank);
+	//printMatrix(localMatrix,N,p);
+
+	// Compute
+	MT_APSP(localMatrix, N, comm, myrank, p);
+	//printf("Result Local Matrix: %d\n", myrank);
+	//printMatrix(localMatrix,N,p);
+	
+	// Gather the result
+	MPI_Gather(localMatrix,	N*N/p, MPI_INT, mat, N*N/p, MPI_INT, 0, comm);
+	if (myrank == 0)
+		gettimeofday(&end,0);
+	MPI_Finalize();
+	
+	// Print out the resulting matrix
+	if (myrank == 0) {
+		//printf("The solution is:\n");
+		//printMatrix(mat,N,1);
+
+		// compare your result with reference result
+		if(CmpArray(mat, ref, N*N))
+			printf("Your result is correct.\n");
+		else
+			printf("Your result is wrong.\n");
+
+		printf("Parallel execution time = %ld usecs\n",
+			(end.tv_sec-start.tv_sec)*1000000+(end.tv_usec-start.tv_usec));
+	}
+}
